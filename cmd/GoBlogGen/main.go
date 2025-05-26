655c3ac04e1fe901c018977b11b8f2be2a7a398d
Harry Day <contact@harryday.xyz>
2025-05-26 14:18:50 +0100

    feat(config): Write initial config parser

--- cmd/GoBlogGen/main.go
+++ cmd/GoBlogGen/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/harrydayexe/GoBlog/internal/gen/config"
+	"github.com/harrydayexe/GoBlog/internal/gen/log"
+)
+
 func main() {
-	println("Hello, World!")
+	// Read Config
+
+	filename := "config.yaml"
+	config, err := config.ParseConfig(filename)
+	if err != nil {
+		panic(err)
+	}
+	// Print Config
+	logger := log.NewCLILogger("main", false)
+
+	logger.Info("Config loaded successfully %+v", config)
 }
---
+++ internal/gen/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/harrydayexe/GoBlog/internal/gen/log"
+	"gopkg.in/yaml.v3"
+)
+
+var logger log.CLILogger = *log.NewCLILogger("CONFIG", false)
+
+// Config represents the configuration structure for the application.
+type Config struct {
+	// InputFolder is the directory where input posts are located.
+	InputFolder string `yaml:"input_folder"`
+
+	// OutputFolder is the directory where the generated website will be placed.
+	OutputFolder string `yaml:"output_folder"`
+}
+
+// ParseConfig reads a YAML configuration file and returns a Config struct.
+func ParseConfig(name string) (Config, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		werr := fmt.Errorf("failed to read config file %s: %w", name, err)
+		logger.Error(werr)
+		return Config{}, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		werr := fmt.Errorf("failed to unmarshal config file %s: %w", name, err)
+		logger.Error(werr)
+		return Config{}, err
+	}
+
+	valErr := cfg.validateConfig()
+	if valErr != nil {
+		werr := fmt.Errorf("config validation failed: %w", valErr)
+		logger.Error(werr)
+		return Config{}, valErr
+	}
+
+	logger.Info("Config file parsed successfully: %+v", name)
+	return cfg, nil
+}
+
+func (cfg *Config) validateConfig() error {
+	if cfg.InputFolder == "" {
+		cfg.InputFolder = "./posts"
+	}
+	if cfg.OutputFolder == "" {
+		cfg.OutputFolder = "./site"
+	}
+	if cfg.InputFolder == cfg.OutputFolder {
+		return fmt.Errorf("input_folder and output_folder cannot be the same")
+	}
+	return nil
+}
