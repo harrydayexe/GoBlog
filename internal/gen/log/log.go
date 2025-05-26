a9c8cb86ccaa4c25b91c9613c8b3557cd5ef7404
Harry Day <contact@harryday.xyz>
2025-05-26 14:18:30 +0100

    fix(logger): Change format of logs

--- internal/gen/log/log.go
+++ internal/gen/log/log.go
@@ -14,37 +14,41 @@ type CLILogger struct {
 }
 
 // Info outputs an info log to the stdout
-func (l CLILogger) Info(key string, format string, args ...any) {
-	p := fmt.Sprintf("\033[36m%"+l.width+"s \033[90m:\033[0m ", key)
+func (l CLILogger) Info(format string, args ...any) {
+	key := fmt.Sprintf("INFO (%s) ", l.key)
+	p := fmt.Sprintf("\033[36m%-"+l.width+"s \033[90m:\033[0m ", key)
 	fmt.Printf(p+format+"\n", args...)
 }
 
 // Debug outputs a debug log to the stdout if verbose mode is enabled
-func (l CLILogger) Debug(key string, format string, args ...any) {
+func (l CLILogger) Debug(format string, args ...any) {
 	if l.verbose {
-		p := fmt.Sprintf("\033[96m%"+l.width+"s \033[90m:\033[0m ", key)
+		key := fmt.Sprintf("DEBUG (%s) ", l.key)
+		p := fmt.Sprintf("\033[96m%-"+l.width+"s \033[90m:\033[0m ", key)
 		fmt.Printf(p+format+"\n", args...)
 	}
 }
 
 // Warn outputs a warning log to the stdout
 func (l CLILogger) Warn(format string, args ...any) {
-	p := fmt.Sprintf("\033[33m%"+l.width+"s \033[90m:\033[0m ", "warn")
+	key := fmt.Sprintf("WARN (%s) ", l.key)
+	p := fmt.Sprintf("\033[33m%-"+l.width+"s \033[90m:\033[0m ", key)
 	fmt.Printf(p+format+"\n", args...)
 }
 
 // Error outputs an error log to the stderr
 func (l CLILogger) Error(err error) {
-	p := fmt.Sprintf("\033[31m%"+l.width+"s \033[90m:\033[0m ", "error")
+	key := fmt.Sprintf("ERROR (%s) ", l.key)
+	p := fmt.Sprintf("\033[31m%-"+l.width+"s \033[90m:\033[0m ", key)
 	fmt.Fprintf(os.Stderr, p+"%s\n", err.Error())
 }
 
 // NewCLILogger creates a new instance of CLILogger with the specified key,
 // verbosity and column width.
-func NewCLILogger(key string, verbose bool, width int) *CLILogger {
+func NewCLILogger(key string, verbose bool) *CLILogger {
 	return &CLILogger{
 		verbose: verbose,
-		width:   fmt.Sprintf("%d", width),
+		width:   "15",
 		key:     key,
 	}
 }
