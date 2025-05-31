baf8a9386e5d1db7faa69195b6fab383763db3e4
Harry Day <contact@harryday.xyz>
2025-05-31 14:49:39 +0100

    feat(gencli): Add command line flag for config file name

--- cmd/GoBlogGen/main.go
+++ cmd/GoBlogGen/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/harrydayexe/GoBlog/internal/gen/config"
 	"github.com/harrydayexe/GoBlog/internal/gen/log"
 )
 
 func main() {
+	// Get the config file from the command line
+	cfgFileNamePtr := flag.String("config", "config.yaml", "Path to the configuration file")
+	flag.Parse()
+
 	// Read Config
 
-	filename := "config.yaml"
-	config, err := config.ParseConfig(filename)
+	config, err := config.ParseConfig(*cfgFileNamePtr)
 	if err != nil {
 		panic(err)
 	}
