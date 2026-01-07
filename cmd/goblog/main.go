70b8f4e1e531ddd8e72872931b57e06d3f8409d8
Harry Day <contact@harryday.xyz>
2026-01-07 21:35:57 +0000

    build: add go releaser config

--- cmd/goblog/main.go
+++ cmd/goblog/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+// These are replaced at build time by GoReleaser
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
-	println("Hello, World!")
+	// Add a version command
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Printf("goblog %s\n", version)
+		fmt.Printf("commit: %s\n", commit)
+		fmt.Printf("built: %s\n", date)
+		os.Exit(0)
+	} else {
+		println("Hello, World!")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/harrydayexe/GoBlog
 
 go 1.25.4
-
-require github.com/harrydayexe/GoWebUtilities v1.3.0 // indirect
