0f4e0733c54cf3a133c282f5eaab2591220d3ac8
Harry Day <contact@harryday.xyz>
2026-02-19 22:27:54 +0000

    feat(server): finish handler implementation

--- pkg/server/handler.go
+++ pkg/server/handler.go
@@ -41,11 +41,12 @@ func generateHandler(cfg HandlerConfig, blog *generator.GeneratedBlog) http.Hand
 	mux := http.NewServeMux()
 
 	root := fmt.Sprintf("GET %s/", cfg.BlogRoot)
-	mux.Handle(root+"index.html", handleIndex(blog))
+	mux.Handle(root+"posts", handleIndex(blog))
 	mux.Handle(root, handleIndex(blog))
+	mux.Handle(root+"posts/{postName}", handlePost(blog))
 
-	mux.Handle(root+"tags.html", handleTagsIndex(blog))
 	mux.Handle(root+"tags", handleTagsIndex(blog))
+	mux.Handle(root+"tags/{postName}", handlePost(blog))
 
 	return mux
 }
@@ -55,18 +56,35 @@ func handleIndex(blog *generator.GeneratedBlog) http.Handler {
 		w.Write(blog.Index)
 	})
 }
+
 func handlePost(blog *generator.GeneratedBlog) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO: Implement this
+		postName := r.PathValue("postName")
+		bits, prs := blog.Posts[postName]
+		if !prs {
+			w.WriteHeader(404)
+			return
+		}
+
+		w.Write(bits)
 	})
 }
+
 func handleTagsIndex(blog *generator.GeneratedBlog) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write(blog.TagsIndex)
 	})
 }
+
 func handleTag(blog *generator.GeneratedBlog) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO: Implement this
+		postName := r.PathValue("tagName")
+		bits, prs := blog.Tags[postName]
+		if !prs {
+			w.WriteHeader(404)
+			return
+		}
+
+		w.Write(bits)
 	})
 }
